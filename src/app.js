491e21cd4bfcef61d98839c719d087e732e16684
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2016-11-29 22:56:12 +0300

    a few small changes

--- src/app.js
+++ src/app.js
@@ -4,34 +4,24 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope) {
-
     $scope.matrix = {
         data: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-        options: {
-            width: 400,
-            height: 400
-        }
+        options: { width: 400, height: 400 }
     };
 });
 
 app.directive('d3Matrix', function(){
     return {
         restrict: "EA",
-        scope: {
-            data: "=data",
-            options: "=options"
-        },
+        scope: { data: "=data", options: "=options" },
         link: function (scope, element, attrs) {
 
-            var chart = KashMatrix(); // window.KashMatrix
-
-            var svg = d3.select(element[0]).append("svg");
-
+            var chart = window.KashMatrix();
+            var svg = window.d3.select(element[0]).append("svg");
             var render = function () { svg.call(chart) };
 
             scope.$watch('data', function(newVal, oldVal){
-                svg.datum(newVal);
-                render();
+                svg.datum(newVal); render();
             }, true);
 
             scope.$watch('options', function(newVal, oldVal){
